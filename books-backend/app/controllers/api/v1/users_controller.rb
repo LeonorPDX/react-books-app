353bd30f5f00b1e5485cbac7d90346941454f4a0
LeonorPDX <leonoracolbert@gmail.com>
2020-10-28 14:24:05 -0700

    Moved the controller action to remove a book from a users collection to the user controller, using the update action; destroy would no work because the frontend won't have access to the join table and its IDs, only the associated data of users and books.

--- books-backend/app/controllers/api/v1/users_controller.rb
+++ books-backend/app/controllers/api/v1/users_controller.rb
@@ -9,4 +9,15 @@ class Api::V1::UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user, only: [:id, :username], include: [:notes, :books]
     end
+
+    def update # Fetch sends patch request to ../api/v1/users/:id when removing book from 
+        ub = UserBook.find_by(user_id: params[:id], book_id: params[:book_id])  # Could refactor into instance method in the User class, def remove_book(book_id)
+        if ub.destroy
+            book = Book.find(params[:book_id])
+            render json: book # Return a book JSON obj so the fetch can pass it into dispatch to be filtered from userBooks array in Redux store
+        else
+            reder json: {error: "Error removing book from user's collection."}
+        end
+    end
+
 end
