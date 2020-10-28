c94a7ba0d9657f77645c6da3aeacd345cc60f6c1
LeonorPDX <leonoracolbert@gmail.com>
2020-10-28 12:47:28 -0700

    Cleaned up frontend files to removes performance reporting and tests, un-used css files, cleaned up App.js and Index.js

--- books-backend/app/models/book.rb
+++ books-backend/app/models/book.rb
@@ -3,5 +3,8 @@ class Book < ApplicationRecord
     has_many :user_books
     has_many :users, through: :user_books
 
-
+    validates :title, presence: true
+    validates :authors, presence: true
+    validates :img, presence: true
+    validates :description, presence: true
 end
