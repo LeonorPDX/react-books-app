8c821fd1e230c6da26060fb6d676f3e0e7e6f240
LeonorPDX <leonoracolbert@gmail.com>
2020-10-29 11:56:20 -0700

    Added notes in reducer for various actions and what they will need to do

--- books-frontend/src/reducers/reducer.js
+++ books-frontend/src/reducers/reducer.js
@@ -29,6 +29,10 @@ function manageUser(state = {username: "", id: 0, userBooks: [], userNotes: []},
   
           return {...state, username: action.user.username, id: action.user.id, userBooks: action.user.books, userNotes: action.user.notes }
 
+          // case 'ADD_USER_BOOK' returns state and concat action.book onto userBooks
+          // case 'ADD_USER_NOTE' returns state and concat action.note onto userNotes
+          //case 'REMOVE_USER_BOOK' returns statre and filters userBooks for book matching action.book
+
         default:
           return state;
       }
