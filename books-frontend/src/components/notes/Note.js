2e6332afbb1acfbc5df38b0e82d30ff4a6ab414f
LeonorPDX <leonoracolbert@gmail.com>
2020-11-02 10:04:36 -0800

    Minor styling changes to note

--- books-frontend/src/components/notes/Note.js
+++ books-frontend/src/components/notes/Note.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Note = props => {
     return (
+    <div>
     <div className="card">
         <div className="card-body">
             {props.note.content}
         </div>
         <div className="card-footer">
-            {props.bookTitle}
+            <small><i>{props.bookTitle}</i></small>
         </div>
+    </div>
+    <br />
     </div>
     )
 }
