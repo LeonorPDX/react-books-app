56c24f60bb697397e8212c0213989a7ac8e6496e
LeonorPDX <leonoracolbert@gmail.com>
2020-11-03 15:55:00 -0800

    Made User a functional component

--- books-frontend/src/components/users/User.js
+++ books-frontend/src/components/users/User.js
@@ -2,21 +2,18 @@ import React from 'react';
 
 import BookCard from '../books/BookCard'
 
-class User extends React.Component {
+const User = props => {
 
-    render() {
-
-        return (
-            <div className="container">
-        
-            <h4>{this.props.user.username}'s Books</h4>
-            <div className="row row-cols-1 row-cols-md-4">
-                {this.props.user.userBooks.map(book => <BookCard key={book.id} book={book} />)}
-            </div>
+    return (
+    <div className="container">
     
-          </div>
-        )
-    }
+        <h4>{props.user.username}'s Books</h4>
+        <div className="row row-cols-1 row-cols-md-4">
+            {props.user.userBooks.map(book => <BookCard key={book.id} book={book} />)}
+        </div>
+
+    </div>
+    )
 
 }
 
