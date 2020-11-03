f71af5a4e11e83c84d227e73ec5c50b46fac3846
LeonorPDX <leonoracolbert@gmail.com>
2020-11-03 15:48:39 -0800

    Made BookCard a functional component

--- books-frontend/src/components/books/BookCard.js
+++ books-frontend/src/components/books/BookCard.js
@@ -2,25 +2,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 
-class BookCard extends Component {
+const BookCard = props => {
 
+  const { book } = props;
 
-  render() {
-    const { book } = this.props;
-
-    return (
-        <div className="col mb-4">
-            <div className="card h-100">
-            <img src={book.img} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{book.title}</h5>
-                <p className="card-text">{book.authors}</p>
-                <Link to={`/books/${book.id}`} className="btn btn-primary">View Book</Link>
-            </div>
-            </div>
-        </div>
-    );
-  }
+  return (
+      <div className="col mb-4">
+          <div className="card h-100">
+          <img src={book.img} className="card-img-top" alt="..." />
+          <div className="card-body">
+              <h5 className="card-title">{book.title}</h5>
+              <p className="card-text">{book.authors}</p>
+              <Link to={`/books/${book.id}`} className="btn btn-primary">View Book</Link>
+          </div>
+          </div>
+      </div>
+  );
 };
 
 export default BookCard;
