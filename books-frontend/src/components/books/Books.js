a153473003a661f74675e9bfbe7d6e1b7e94e30a
LeonorPDX <leonoracolbert@gmail.com>
2020-11-03 15:50:21 -0800

    Made Books a functional component

--- books-frontend/src/components/books/Books.js
+++ books-frontend/src/components/books/Books.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import BookCard from './BookCard';
 import SearchBooks from './SearchBooks';
 
-class Books extends Component {
+const Books = props => {
 
-
-  render() {
-
-    return(
-      <div className="container">
-        <SearchBooks books={this.props.books} />
-        
-        <h4>All Books</h4>
-        <div className="row row-cols-1 row-cols-md-4">
-            {this.props.books.map(book => <BookCard key={book.id} book={book} />)}
-        </div>
-
+  return(
+    <div className="container">
+      <SearchBooks books={props.books} />
+      
+      <h4>All Books</h4>
+      <div className="row row-cols-1 row-cols-md-4">
+          {props.books.map(book => <BookCard key={book.id} book={book} />)}
       </div>
-    );
-  }
+
+    </div>
+  );
+
 };
 
 export default Books;
